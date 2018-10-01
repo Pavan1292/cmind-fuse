97cfd56aac9018209baee6810a03c7c836c35ff9
Murali Veesambattu <MVeesambattu@simeiosolutions.com>
2018-10-01 07:37:42 +0530

    fuse Base Theme

---
+++ src/app/navigation/navigation.ts
@@ -0,0 +1,30 @@
+import { FuseNavigation } from '@fuse/types';
+
+export const navigation: FuseNavigation[] = [
+    {
+        id       : 'applications',
+        title    : 'Applications',
+        translate: 'NAV.APPLICATIONS',
+        type     : 'group',
+        children : [
+            {
+                id       : 'sample',
+                title    : 'Sample',
+                translate: 'NAV.SAMPLE.TITLE',
+                type     : 'item',
+                icon     : 'email',
+                url      : '/sample'
+               
+            },
+            {
+                id       : 'settings',
+                title    : 'Settings',
+                // translate: 'NAV.SAMPLE.TITLE',
+                type     : 'item',
+                icon     : 'settings',
+                url      : '/settings'
+               
+            }
+        ]
+    }
+];
